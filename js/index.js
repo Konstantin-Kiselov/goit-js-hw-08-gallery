deb3830cd54fd60b180be6c2eef79eaab750d763
Konstantin-Kiselov <constantine.kiss@gmail.com>
2021-07-12 23:14:04 +0300

    сделал изменения index.js
    после вебинара Репеты

--- js/index.js
+++ js/index.js
@@ -1,19 +1,26 @@
 import { galleryItems } from './app.js';
 
+
 const refs = {
     gallery: document.querySelector('.js-gallery'),
     modalCloseButton: document.querySelector('[data-action="close-lightbox"]'),
-    modal: document.querySelector('.js-lightbox'),
+    modalOpen: document.querySelector('.js-lightbox'),
     overlay: document.querySelector('.lightbox__overlay'),
     modalImage: document.querySelector('.lightbox__image'),
 };
+// console.dir(refs);
 
-const showImages = galleryItems.reduce(
-    (str, { preview, original, description }) =>
-        str +
-        `<li class="gallery__item">
-  
-    <a class="gallery__link" href=${original}>
+refs.gallery.insertAdjacentHTML('afterbegin', createGalleryMarkup(galleryItems));
+
+refs.gallery.addEventListener('click', onGalleryCardClick);
+
+function createGalleryMarkup(galleryItems) {
+  return galleryItems.map(({ preview, original, description }) => {
+      return `<li class="gallery__item">
+    <a
+      class="gallery__link"
+      href="${original}"
+    >
     <img
       class="gallery__image"
       src="${preview}"
@@ -21,51 +28,47 @@ const showImages = galleryItems.reduce(
       alt="${description}"
     />
     </a>
- 
-   </li>
-`,
-    '',
-);
+    </li>`;
+  }).join('');
+};
 
-function ModalIsOpen(target) {
-    refs.modal.classList.add('is-open');
-    refs.overlay.addEventListener('click', ModalIsClose);
-    window.addEventListener('keydown', onModalPress);
-    refs.modalCloseButton.addEventListener('click', ModalIsClose);
+function onGalleryCardClick(event) {
+  let imageElement;
+  const isLinkElement = event.target.classList.contains('gallery__image');
+  if (!isLinkElement) {
+    return;
+  }
 
-    galleryItems.forEach(({ preview, original, description }) => {
-        if (target.src === preview) {
-            refs.modalImage.src = `${original}`;
-            refs.modalImage.alt = `${description}`;
-        }
-    });
-}
+  //запретить браузеру переходить по ссылке картинки
+  event.preventDefault();
 
-function ModalIsClose() {
-    refs.modal.classList.remove('is-open');
-    refs.modalImage.src = '';
-    refs.modalImage.alt = '';
-    refs.overlay.removeEventListener('click', ModalIsClose);
-    refs.modalCloseButton.removeEventListener('click', ModalIsClose);
+  //получение url большого изображения
+  imageElement = event.target;
+  //console.log(imageElement);
+  const originalImage = imageElement.getAttribute('data-source');
+  
+  //Подмена значения атрибута src элемента img.lightbox__image.
+  refs.modalImage.src = originalImage;
 
-}
+  //открытие модального окна
+  onModalOpen();
+};
 
-function onModalPress(event) {
-    const target = event;
-    if (event.key === 'Escape') {
-        ModalIsClose();
-    }
-}
+function onModalOpen() {
+  refs.modalOpen.classList.add('is-open');
+    
+// Закрытие модального окна по клику на div.lightbox__overlay.
+  refs.overlay.addEventListener('click', onCloseModal);
+    
+// Закрытие модального окна по клику на кнопку button[data-action="close-lightbox"]
+  refs.modalCloseButton.addEventListener('click', onCloseModal);
+};
 
-refs.gallery.insertAdjacentHTML('afterbegin', showImages);
+function onCloseModal() {
+  refs.modalOpen.classList.remove('is-open');
+  refs.overlay.removeEventListener('click', onCloseModal);
+  refs.modalCloseButton.removeEventListener('click', onCloseModal);
 
-refs.gallery.addEventListener('click', (event) => {
-    event.preventDefault();
-    const target = event.target;
-
-    if (event.target.nodeName !== 'IMG') {
-        return;
-    }
-
-    ModalIsOpen(target);
-});
+  // Очистка значения атрибута src элемента img.lightbox__image. Это необходимо для того, чтобы при следующем открытии модального окна, пока грузится изображение, мы не видели предыдущее.
+  refs.modalImage.src = '';
+};
